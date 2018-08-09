37e5ec17eadb78fa1931c8bd0ff5929b686d2db7
Daniel Dias Costa <Daniel Dias@prodespluz.local>
2018-08-09 14:53:15 -0300

    Utilizar api json de dicionario a partir do angularjs.

---
+++ dicionario/app/app.js
@@ -0,0 +1,36 @@
+angular.module('app', [])
+.controller('myController', [
+		'$scope','$http',
+  	function($scope, $http) {
+				$scope.procura = function(input) {
+						def.innerHTML = '';
+					  $scope.model.msg = 'Aguarde...';
+						var url = 'http://dicionario-aberto.net/search-json/' + $scope.model.palavra;
+						$http.get(url)
+						.then(function(response) {
+							  console.log(response);
+								$scope.model.msg = null;
+								if(typeof response.data.superEntry === 'undefined') {
+										def.innerHTML += 'Def.';
+										for(i in response.data.entry.sense) {
+												def.innerHTML += '<br>' + (response.data.entry.sense[i].def).replace(/_/g, '"');
+										}
+								} else {
+										for(i in response.data.superEntry) {
+												if(parseInt(i) === 0)
+														def.innerHTML += 'Def.' + (parseInt(i)+1) + '<br>';
+												else
+														def.innerHTML += '<br><br>Def.' + (parseInt(i)+1) + '<br>';
+												for(j in response.data.superEntry[i].entry.sense) {
+														def.innerHTML += '<br>' + (response.data.superEntry[i].entry.sense[j].def).replace(/_/g, '"');
+												}
+										}
+								}
+						}, function(error) {
+							  $scope.model.msg = 'Ocorreu um erro.';
+								console.log(error)
+								console.log(url);
+						});      
+				}	
+  	}
+])
